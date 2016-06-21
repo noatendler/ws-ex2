2df54d5879da9ae68b4143d2dfb1989bf8a09b4f
noatendler <noatendler1@gmail.com>
2016-06-21 13:29:42 +0300

    Update allstudents.js

--- client/public/js/allstudents.js
+++ client/public/js/allstudents.js
@@ -1,7 +1,7 @@
 var showApp = angular.module('showApp',[]);
 
 showApp.controller('studentCtrl',['$scope','$http',function($scope,$http) {
-     $http.get("http://localhost:3000/showall").success(function(data){
+     $http.get("https://ws-noa-ex2.herokuapp.com/showall").success(function(data){
         $scope.allstudents = data; 
         console.log(data);
     });
